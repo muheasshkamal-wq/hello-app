539ae6332602795b2e24181dbeccf61af74c6b2d
Muheassh <muheasshkamal@gmail.com>
2026-03-18 10:46:24 +0530

    UC2

---
+++ oopsBannerAppuc2.java
@@ -0,0 +1,18 @@
+package com.srm.helloapp;
+
+public class oopsBannerAppuc2 {
+	public static void main(String[] args) {
+
+        System.out.println("   ***    ***    *****     ****   ");
+        System.out.println("  ** **  **  **  **  **  **       ");
+        System.out.println(" **   ** **   ** **  **  **       ");
+        System.out.println(" **   ** **   ** *****     *****  ");
+        System.out.println(" **   ** **   ** **             **");
+        System.out.println("  ** **  ** **   **             **");
+        System.out.println("   ***    ***    **        *****  ");
+
+    }
+
+}
+
+
